1be5364c2baceb06107635e8c3147dea83de2851
kaneraZh <ari1337ferrada@gmail.com>
2023-10-29 21:13:05 -0300

    models
    models made :D

--- portal/models.py
+++ portal/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+    def __str__(self):return self.name
+    def get_absolute_url(self):return reverse("item_detail", kwargs={"pk": self.pk})
+    nombre  = models.CharField(max_length=20)
+    precio  = models.IntegerField()
+
+class Producto(models.Model):
+    def __str__(self):return self.name
+    def get_absolute_url(self):return reverse("producto_detail", kwargs={"pk": self.pk})
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    stock= models.IntegerField()
+
+class Servicio(models.Model):
+    def __str__(self):return self.name
+    def get_absolute_url(self):return reverse("servicio_detail", kwargs={"pk": self.pk})
+    item    = models.ForeignKey(Item,on_delete=models.CASCADE)
+    tiempo  = models.TimeField(auto_now=False, auto_now_add=False)
+
+class Persona(models.Model):
+    def __str__(self):return self.name
+    def get_absolute_url(self):return reverse("persona_detail", kwargs={"pk": self.pk})
+    nombre      = models.CharField(max_length=20)
+    correo      = models.CharField(max_length=50)
+    productos   = models.ManyToManyField(Producto)
+    servicios   = models.ManyToManyField(Servicio)

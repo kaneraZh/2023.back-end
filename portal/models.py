3ebb2e78ba92cac636d4a0858d3acca7fd62f9f3
kaneraZh <ari1337ferrada@gmail.com>
2023-11-01 23:13:05 -0300

    html & view functions
    html hechos y funciones de view para llamarles

--- portal/models.py
+++ portal/models.py
@@ -1,24 +1,19 @@
 from django.db import models
 
 class Item(models.Model):
-    #def __str__(self):return self.name
+    def __str__(self):return f'{nombre}, {precio}, {descripcion}'
     def get_absolute_url(self):return reverse("item_detail", kwargs={"pk": self.pk})
-    #class Meta:
-    #    abstract = True
     nombre      = models.CharField(max_length=20)
     precio      = models.IntegerField()
     descripcion = models.CharField(max_length=150, default="")
-
 class Producto(Item):
     def __str__(self):return f'{self.nombre}, {self.precio}, {self.descripcion}, {self.stock}'
     def get_absolute_url(self):return reverse("producto_detail", kwargs={"pk": self.pk})
-    stock= models.IntegerField()
-
+    stock = models.IntegerField()
 class Servicio(Item):
     def __str__(self):return f'{self.nombre}, {self.precio}, {self.descripcion}, {self.tiempo}'
     def get_absolute_url(self):return reverse("servicio_detail", kwargs={"pk": self.pk})
-    tiempo  = models.TimeField(auto_now=False, auto_now_add=False)
-    
+    tiempo = models.TimeField(auto_now=False, auto_now_add=False)
 class Persona(models.Model):
     def __str__(self):return f'{self.nombre}, {self.correo}, productos: {self.productos.count()}, servicios: {self.servicios.count()}'
     def get_absolute_url(self):return reverse("persona_detail", kwargs={"pk": self.pk})
--- portal/views.py
+++ portal/views.py
@@ -1,3 +1,47 @@
+from django.template.loader import render_to_string
+def navbar(ventana:int = 0)->str:
+    data = {'ventana' : clamp(ventana, 0, 2)}
+    return render_to_string('items/navbar.html', data)
+def tables(headings:list, columns:list, styles:list):
+    data = {}
+    data['headings'] = headings
+    class column:
+        style:int
+        title:str
+        contents:list
+        def __init__(self, style:int, contents:list):
+            self.style = style
+            self.title = contents.pop(0)
+            self.contents = contents
+    cols = []
+    for i in range(len(columns)):
+        stl:int = i
+        while(stl >= len(style)):
+            stl-= len(style)
+        cols.append(stl, columns[i])
+    data['columns'] = cols
+    return render_to_string('items/tables.html', data)
+def tabs(titles:list, contents:list):
+    data = {'selected':titles[0]}
+    class item:
+        title:str
+        content:str
+        def __init__(self, title:str, content:str):
+            self.title = title
+            self.content = content
+    items:list = []
+    for i in range(len(titles)):
+        items.append( item(titles[i], contents[i]) )
+    data['items'] = items
+    return render_to_string('items/tabs.html', data)
+def tarjeta(cabecera:str, titulo:str, descripcion:str)->str:
+    data = {}
+    data['cabecera']    = cabecera
+    data['titulo']      = titulo
+    data['descripcion'] = descripcion
+    return render_to_string('items/tarjeta.html', data)
+
+
 from django.shortcuts import render
 import portal.models as models
 def main(request):
@@ -5,3 +49,29 @@ def main(request):
     data['productos'] = models.Producto.objects.all()
     data['servicios'] = models.Servicio.objects.all()
     return render(request, 'main.html', data)
+
+def productos(request, pagina:int=0):
+    data = {}
+    # get productos desde 8*pagina hasta 8*pagina+8?
+    # para 8 items en total
+    # if aprieta producto, lleva al detalle de ese producto
+    return render(request, 'productos.html', data)
+
+def servicios(request, pagina:int=0):
+    data = {}
+    # get servicios desde 8*pagina hasta 8*pagina+8?
+    # para 8 items en total
+    # if aprieta producto, lleva al detalle de ese servicio
+    return render(request, 'servicios.html', data)
+
+def detalles(request, model):
+    data = {}
+    #data['modelo'] = 
+    return render(request, 'detalle.html', data)
+
+def persona(request, persona:str=''):
+    data = {}
+    data['persona'] = models.Persona.objects.get(persona)
+    #data['productos'] = models.Producto.objects.all() productos de la persona
+    #data['servicios'] = models.Servicio.objects.has() servicios de la persona
+    return render(request, 'persona.html', data)
